bd8d1dc9ecb746ebbecc0e54d93d3495c6db0d9a
YukiTsunemori <yuki.tsunemori1229@gmail.com>
2025-11-14 19:01:30 +0900

    completed_atをdateからdatetimeに変更

--- backend/db/migrate/20251114091347_create_tasks.rb
+++ backend/db/migrate/20251114091347_create_tasks.rb
@@ -3,7 +3,7 @@ class CreateTasks < ActiveRecord::Migration[8.0]
     create_table :tasks do |t|
       t.string :name
       t.text :memo
-      t.date :completed_at
+      t.datetime :completed_at
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
