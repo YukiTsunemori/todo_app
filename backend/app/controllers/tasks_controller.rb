840c4467cda94c369e2b90d0cce49da263ecf613
YukiTsunemori <yuki.tsunemori1229@gmail.com>
2025-11-19 20:40:24 +0900

    index以外のアクションを削除
    Removed create, edit, update, and destroy methods from tasks_controller.

--- backend/app/controllers/tasks_controller.rb
+++ backend/app/controllers/tasks_controller.rb
@@ -4,24 +4,6 @@ class TasksController < ApplicationController
     render json: @tasks
   end
 
-  def create
-    @task = Task.new(todo_params)
-    if @task.save
-      render json: @task, status: :created
-    else
-      render json: @task, status: :unprocessable_content
-    end
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
   private
   def todo_params
      params.require(:task).permit(:name, :user_id, :deadline_at)
