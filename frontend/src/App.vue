3c9854e7522ab771dadf354f5867f2ae85b47b6b
YukiTsunemori <yuki.tsunemori1229@gmail.com>
2025-11-23 20:34:36 +0900

    TaskFormコンポーネントを削除
    TaskFormは別PRでcommitします。

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div id="app">
-    <!-- <router-view /> -->
-    <TaskForm />
     <TaskIndex />
   </div>
 </template>
 
 <script>
-import TaskForm from '@/components/TaskForm.vue'
 import TaskIndex from '@/components/TaskIndex.vue'
 export default {
   name: 'App',
   components: {
-    TaskForm,
     TaskIndex
   }
 }
