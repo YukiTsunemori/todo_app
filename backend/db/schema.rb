e8c67cb7f5d7e9bccf1463c97660852106e96b3f
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-11-28 18:54:27 +0900

    rails db:migrate:resetを実行し、schemaを最新の状態に更新しました。

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -41,7 +41,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_28_070012) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
+    t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
